c4bdd2037a825d93411ac42260c756604bd61ce1
rifkegribenes <rifkegribenes@gmail.com>
2018-10-06 03:49:28 +0000

    mongoose models

--- models/thread.js
+++ models/thread.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ReplySchema = new Schema({
+  text: { type: String, required: true },
+  delete_password: { type: String, required: true },
+  threadId: { type: Schema.Types.ObjectId, required: true }
+});
+
 const ThreadSchema = new Schema({
   text: { type: String, required: true },
   delete_password: { type: String, required: true },
-  created_on: String,
-  bumped_on: 
-  likeIPs: [{ type: String }]
+  created_on: Date,
+  bumped_on: Date,
+  replies: [ReplySchema]
 });
 
 module.exports = mongoose.model('Thread', ThreadSchema);
